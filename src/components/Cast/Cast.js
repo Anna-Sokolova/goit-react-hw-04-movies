a287dde1bb5ae09e65fea60ec3c126b2f46c9088
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-02 15:42:23 +0200

    Added default img

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -15,23 +15,28 @@ class Cast extends Component {
     );
     const result = await response.json();
     const cast = result.cast;
+
     this.setState({ cast: [...cast] });
-    console.log(this.state.cast);
+    // console.log(this.state.cast);
   }
 
   render() {
+    const { cast } = this.state;
     return (
       <>
         <Title title="В фильме снимались:" />
 
         <ul className={styles.listCast}>
-          {this.state.cast &&
-            this.state.cast.map(({ id, profile_path, name, character }) => {
+          {cast &&
+            cast.map(({ id, profile_path, name, character }) => {
               const imgPosterUrl = `https://image.tmdb.org/t/p/w92/${profile_path}`;
 
               return (
                 <li key={id} className={styles.itemCast}>
-                  <img src={imgPosterUrl} alt={name} />
+                  <img
+                    src={profile_path ? imgPosterUrl : `${defaultImg}`}
+                    alt={name}
+                  />
                   <div className={styles.cardContent}>
                     <p className={styles.Actor}>{name}</p>
                     <p className={styles.Character}>{character}</p>
@@ -45,8 +50,9 @@ class Cast extends Component {
   }
 }
 
-// Cast.defaultProps = {
-//   imgPosterUrl: defaultImg,
-// };
+Cast.defaultProps = {
+  profile_path: defaultImg,
+};
+
 
 export default Cast;
